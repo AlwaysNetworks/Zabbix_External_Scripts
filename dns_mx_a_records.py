68de8a5e7c792dcd671b1f131f9539387ccb0ac3
Nick Shaw <nick@alwaysnetworks.co.uk>
2023-05-12 18:23:27 +0100

    move to query from resolve for zabbix server

--- dns_mx_a_records.py
+++ dns_mx_a_records.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Script to verify that all MX record for a domain resolve to A records.
+"""
+
 import dns.resolver
 import sys
 
@@ -7,14 +11,14 @@ domain = sys.argv[1]
 # Resolve MX records for the domain
 resolver = dns.resolver.Resolver()
 try:
-  nameserver = resolver.resolve(domain, "NS")[0].to_text()
-  resolver.nameservers = [resolver.resolve(nameserver, "a")[0].to_text()]
+  nameserver = resolver.query(domain, "NS")[0].to_text()
+  resolver.nameservers = [resolver.query(nameserver, "a")[0].to_text()]
 except:
   print("Fail: Cannot find nameservers for domain")
   sys.exit(1)
 
 try:
-  mx_records = resolver.resolve(domain, "MX")
+  mx_records = resolver.query(domain, "MX")
 except:
   print("Fail: Cannot find mx record for domain")
   sys.exit(1)
@@ -27,7 +31,7 @@ for mx_record in mx_records:
     hostname = str(mx_record.exchange)
     # Resolve A records for the hostname
     try:
-      a_records = dns.resolver.resolve(hostname, "A")
+      a_records = dns.resolver.query(hostname, "A")
     except:
       all_mx_have_a_records = False
       break
